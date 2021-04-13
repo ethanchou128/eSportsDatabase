5367dd1da8f93c220797e6c88521c19485fa3546
epicSpark <justinchiang707@gmail.com>
2021-04-12 19:57:47 -0700

    worked on ncurses

--- Design.cpp
+++ Design.cpp
@@ -7,69 +7,52 @@
 using namespace std;
 
 /////////////////Menu/////////////////////
-Menu::Menu(string text, char trigger, string* items, int num_items){
+Menu::Menu(string text, char trigger){
     this->text = text;
     this->trigger = trigger;
-    this->items = items;
-    this->num_items = num_items;
-    this->selected_item = 0;
 }
 
-void Menu::selectNextItem(){
-    selected_item++;
-    if (selected_item >= num_items){
-        selected_item = 0;
-    }
-}
-
-void Menu::selectPreviousItem(){
-    selected_item--;
-    if (selected_item < 0 ){
-        selected_item = num_items - 1;
-    }
-}
-
-////////////////MenuBar////////////////////////
-MenuBar::MenuBar(WINDOW* win, Menu* menus, int num_menus){
+////////////////MenuOut///////////////////
+MenuOut::MenuOut(WINDOW* win, Menu* menus, int num_menus){
     this->win = win;
     this->menus = menus;
     this->num_menus = num_menus;
-    this->selected_menu = -1;
 
-    int yMax, xMax, yBeg, xBeg;
-    getmaxyx(win, yMax, xMax);
-    getbegyx(win , yBeg, xBeg);
+    int start = win->_maxx / 6;
+    int current_pos = start;
 
-    menuwin = newwin(yMax - 2, xMax - 2, yBeg + 1, xBeg + 1);
-    keypad(menuwin, true);
-    wrefresh(win);
-
-    int current_pos = 2;
-
-    for(int i = 0; i < num_menus; i++){
-        this-> menus[i].start_x = current_pos;
-        current_pos += this-> menus[i].text.length() + 1;
+    for (int i = 0; i < num_menus; i++){
+        this->menus[i].start_x = current_pos;
+        if (num_menus / 2  == i + 1){
+            current_pos +=  start - this->menus[i + 1].text.length()/2;
+        } else {
+            current_pos += start;
+        }
     }
-
 }
 
-void MenuBar::reset(){
+void MenuOut::reset(){
     for (int i = 0; i < num_menus; i++){
         int start_x = this->menus[i].start_x;
-        string text = this->menus[i].text;        
+        string text = this->menus[i].text;
         mvwprintw(win, 0, start_x, text.c_str());
     }
     wrefresh(win);
 }
 
-void MenuBar::draw(){
+void MenuOut::draw(){
     for (int i = 0; i < num_menus; i++){
         drawMenu(menus[i], selected_menu == i);
     }
+
+    int center = win->_maxx / 2;
+    int adj = center;
+
+    mvwprintw(win, 5, adj, "c");
     selected_menu = -1;
 }
 
-void MenuBar::drawMenu(Menu menu, bool is_selected){
+void MenuOut::drawMenu(Menu menu, bool is_selected){
     int start_x = menu.start_x;
     string text = menu.text;
     if (is_selected){
@@ -78,48 +61,15 @@ void MenuBar::drawMenu(Menu menu, bool is_selected){
     mvwprintw(win, 0, start_x, text.c_str());
     wattroff(win, A_STANDOUT);
     wrefresh(win);
-
-    int ch;
-    drawMenuItems(menu);
-    wrefresh(win);
-    while (is_selected && (ch = wgetch(menuwin)))
-    {
-        switch (ch){
-            case KEY_DOWN:
-                menu.selectNextItem();
-                break;
-            case KEY_UP:
-                menu.selectPreviousItem();
-                break;
-            default:
-                is_selected = false;
-        }
-        drawMenuItems(menu);
-    }
-    werase(menuwin);
-    wrefresh(menuwin);
-    reset();
 }
 
-void MenuBar::drawMenuItems(Menu menu){
-    int yMax, xMax;
-    getmaxyx(menuwin, yMax, xMax);
-    (void)yMax;
-    for (int i = 0; i < menu.num_items; i++){
-        mvwprintw(menuwin, i, 0, menu.items[i].c_str());
-        if (menu.selected_item == i){
-            mvwchgat(menuwin, i, 0, xMax, A_NORMAL, 1, NULL);
-        } else {
-            mvwchgat(menuwin, i, 0, xMax, A_STANDOUT, 1, NULL);
-        }
-    }
-}
-
-void MenuBar::handleTrigger(char trigger){
+void MenuOut::handleTrigger(char trigger){
     for (int i = 0; i < num_menus; i++){
         if (trigger == this->menus[i].trigger){
             selected_menu = i;
         }
     }
 }
+
+
 
---
+++ README.txt
@@ -0,0 +1,38 @@
+CMPT 135 Assignment 5
+=====================
+
+Names of Team Members
+---------------------
+
+(include the full names, SFU email addresses, and SFU student numbers of all
+team members)
+
+Justin Chiang, jpc13@sfu.ca, 301456431
+
+
+Instructions for Compiling and Running
+--------------------------------------
+
+(put instructions here for compiling and running your program; hopefully it's
+just to type "make" in the shell!)
+
+    You can just type "make" into the shell and it will create a executable called "test"
+which you can call by "./test"
+
+
+Limitations
+-----------
+
+(if you didn't implement all requested features, list which ones are missing)
+
+
+Known Bugs
+----------
+
+(list all known bugs here)
+
+
+Extra Features
+--------------
+
+(if your program has any extra or special features, tell us about them here)
